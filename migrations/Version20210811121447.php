de543b577c2529f8ee4848e1de456962b7f2f114
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2021-11-28 23:11:18 +0100

    migrations updated

--- migrations/Version20210811121447.php
+++ migrations/Version20210811121447.php
@@ -20,7 +20,11 @@ final class Version20210811121447 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD is_best BOOLEAN NOT NULL');
+        $isBest ='FALSE';
+        $this->addSql('ALTER TABLE product ADD is_best BOOLEAN');
+        $this->addSql('UPDATE product SET is_best = :isBest WHERE is_best IS NULL',[$isBest]);
+        $this->addSql('ALTER TABLE product ALTER COLUMN is_best SET NOT NULL');
+        
     }
 
     public function down(Schema $schema): void
